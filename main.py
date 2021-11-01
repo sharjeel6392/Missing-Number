dbb2e7f18813e45a67205310342be3706008d645
Sharjeel Ansari <sharjeel6392@gmail.com>
2021-11-01 00:38:23 -0400

    Create main.py
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.

---
+++ main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        N = len(nums)
+        sumExp = N*(N + 1) // 2
+        sumAct = sum(nums)
+        
+        return sumExp - sumAct
